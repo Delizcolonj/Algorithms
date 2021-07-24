8c48c48ab88c80a307e99dda80184957ce211d2f
Delizcolonj <43044663+Delizcolonj@users.noreply.github.com>
2021-07-24 11:57:14 -0400

    Update Point2D.java

--- Lab5BruteForceClosestPair/Point2D.java
+++ Lab5BruteForceClosestPair/Point2D.java
@@ -1,29 +1,38 @@
-package Lab5;
+package lab5;
 
-public class Point2D {
-	public final double x;
-	public final double y;
+public class Point {
+    public Point[] listofPoints;
+    public int numberofPoints;
+    public final double x;
+    public final double y;
 
-	public Point2D(double x, double y) {
-		this.x = x;
-		this.y = y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	}
-	public double getX() {
-		return x;
-	}
+    public double getX() {
+        return x;
+    }
 
-	public double getY() {
-		return y;
-	}
-	public String toString() {
-		return "(" + x + "," +  y + ")";
-	}
+    public double getY() {
+        return y;
+    }
 
-	public static double distance(Point2D p1, Point2D p2) {
-		double distance;
-		distance = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-		return distance;
-	}
-
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+    public static double distanceForm(Point p1, Point p2) {
+        double distance;
+        distance =  Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+        return distance;
+    }
+    public void add(Point p1) { //add method to generate a list of points
+         Point point = new Point(p1.x, p1.y);
+         listofPoints[numberofPoints] = point;
+    }
 }
