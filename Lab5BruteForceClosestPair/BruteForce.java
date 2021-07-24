fba04cdceb7fa8223352bc0cf9a90e1a086c80ce
Delizcolonj <43044663+Delizcolonj@users.noreply.github.com>
2021-07-24 11:56:23 -0400

    Update BruteForce.java

--- Lab5BruteForceClosestPair/BruteForce.java
+++ Lab5BruteForceClosestPair/BruteForce.java
@@ -1,64 +1,73 @@
-package Lab5;
+package lab5;
 
-public class Lab5 {
+import static lab5.Point.*;
 
-	public static double BruteForceClosestPair(Point2D[] list) {
-		double d = 50000000;
-		for (int i = 0; i < list.length - 1; i++) {
-			for (int j = i + 1; j < list.length; j++) {
-				 d = Math.min(d, Point2D.distance(list[i], list[j]));
-			}
-		}
-		return d;
-	}
+public class lab5partA {
+    public static double BruteForceClosestPair(Point[] list) {
+        double d = 500000;//infinity
+        for(int i = 1; i < list.length -1; i++) {
+            for(int j = i + 1; j < list.length; j++) {
+                d = Math.min(d, distanceForm(list[i], list[j]));
+            }
+        }
+        return d;
+    }
+    public static Point[] BruteForceConvexHull(Point[] list){
+        Point[] extremePoints = new Point[10];
+        double a = 0, b = 0, c = 0, check;
+        boolean foundproblem  = true;
 
-	public static void BruteForceConvexHull(Point2D[] list) {
-		Point2D[] extremePoints = null;
+        for (int i = 0; i < list.length; i++) {
+            for (int j = i + 1; j < list.length; j++) {
+                Point p1, p2, p3;
+                p1 = list[i];
+                p2 = list[j];
+                int sign = 0; // check if integer negative or positive
+                a = p2.y - p1.y; //equation for the line
+                b = p1.x - p2.x;
+                c = p1.x * p2.y - p1.y * p2.x;
+                foundproblem = false;
+                for(int k = j + 1; k < list.length; k++) {
+                    p3 = list[k];
+                    check = a * p3.x + b * p3.y - c; // check plugs in the 3rd point and determines if check is all positive or all negative
+                    if (sign != 0) {
+                        if (check > 0 && sign < 0) {
+                            foundproblem = true;
+                            break;
+                        } else if (check < 0 && sign > 0 ) {
+                            foundproblem = false;
+                            break;
+                        }
+                    } else {
+                        if (check > 0) {
+                            sign = 1;
+                        } else if (check < 0) {
+                            sign = -1;
+                        }
+                    }
 
-		double a = 0, b = 0, c = 0, check;
+                } if(foundproblem) {//adds the points when we know p1 and p2 are extreme values
+                    extremePoints.equals(p1);
+                    extremePoints.equals(p2);
+                }
+            }
+        }
+        return extremePoints;
+    }
+    public static void main(String[] args) {
 
-		boolean foundproblem = true;
+        Point[] points = new Point[3];
 
-		for (int i = 0; i < list.length; i++) {
-			for (int j = i + 1; i < list.length; i++) {
-				Point2D p1, p2, p3;
-				int sign = 0;
-				p1 = list[i];// first point
-				p2 = list[j];// second point
-				a = p2.y - p1.y;
-				b = p1.x - p2.x;
-				c = p1.x * p2.y - p1.y * p2.x;
-				foundproblem = false;
-				for (int k = j + 1; i <= list.length; i++) {
-					p3 = list[k];// 3rd point
-					check = a * p3.x + b * p3.y - c;
-					if (sign != 0) {
-						foundproblem = true;
-						break;
-					}
-				}
-				if (!foundproblem) {
+        Point p1 = new Point(5.0, 4.0);
+        Point p2 = new Point(10.0, 5.0);
+        Point p3 = new Point(27.0, 28.0);
 
-				}
-			}
-		}
-	}
+        points[0] = p1;
+        points[1] = p2;
+        points[2] = p3;
 
-	public static void main(String[] args) {
+        System.out.println((p1));
+        System.out.println(BruteForceClosestPair(points));
 
-		Point2D[] points = new Point2D[3];
-
-		Point2D p1 = new Point2D(5.0, 4.0);
-		Point2D p2 = new Point2D(10.0, 5.0);
-		Point2D p3 = new Point2D(27.0, 28.0);
-
-		points[0] = p1;
-		points[1] = p2;
-		points[2] = p3;
-
-		System.out.println((p1));
-		System.out.println(BruteForceClosestPair(points));
-
-	}
-
+    }
 }
